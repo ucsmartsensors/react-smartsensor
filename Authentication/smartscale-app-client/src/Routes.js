299a06ae48d7f92058466398ecaf2dd302ac40c1
ucsmartsensors <35382097+ucsmartsensors@users.noreply.github.com>
2018-01-15 17:45:51 -0500

    auth component
    auth components uploaded by Noah

---
+++ Authentication/smartscale-app-client/src/Routes.js
@@ -0,0 +1,21 @@
+import Login from "./containers/Login";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
+import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
+export default ({ childProps }) =>
+<Switch>
+  <AppliedRoute path="/" exact component={Home} props={childProps} />
+  <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+  <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+{ /* <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} /> */}
+
+  { /* Finally, catch all unmatched routes */ }
+  <Route component={NotFound} />
+</Switch>;
+
---
+++ Authentication/smartscale-app-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>SmartSensor</h1>
+          <p>A Smart Order Fullfillment App</p>
+        </div>
+      </div>
+    );
+  }
+}
