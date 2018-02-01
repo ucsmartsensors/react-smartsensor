77402b54616a262cadbe3c8156842b5f6cbd10b8
Noah L <larsonnh@mail.uc.edu>
2018-02-01 18:48:29 -0500

    lambda function

---
+++ serverless/create.js
@@ -0,0 +1,23 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "shipments",
+    Item: {
+      shippingId: data.content,
+      address: data.address,
+      city: data.city,
+      zip: data.zip,
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
