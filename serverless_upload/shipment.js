3fa9601005ad5733cb209b792220487357155aa4
Noah L <larsonnh@mail.uc.edu>
2018-02-25 10:05:40 -0500

    lamda shipment

--- serverless_upload/shipment.js
+++ serverless_upload/shipment.js
@@ -5,48 +5,60 @@ var shippo = require('shippo')('shippo_test_b19b2d39541b35ca70355a176e82c0840694
 
 
 /*
-
-
 shippingId 
 measuered qty = measuredWeight/weight
- 
-Return PUT countData/measuredWeight
-
-
+ Return PUT countData/measuredWeight
 */
 
 
 
+//create shipment function
+//event is the JSON payload sent from client (i.e. client triggered an event)
+//callback is the response to client
 export async function create(event, context, callback) {
+  
+  //JSON stored in reqData from API POST event from client
   const reqData = JSON.parse(event.body);
   // {parcel, addressTo, addressFrom}
 
   try {
-    //await dynamoDbLib.call("put", params);
+    //create shipment object based on data sent from client
     const shipment = await shippo.shipment.create({
       "address_from": reqData.addressFrom,
       "address_to": reqData.addressTo,
       "parcels": [reqData.parcel],
       "async": false
     })
+
+    //on successful API call, send back response of shipment.rates to client
     callback(null, success(shipment.rates));
   } catch (e) {
     callback(null, failure({ status: false }));
   }
 }
 
+//Transaction function
+//event is the JSON payload sent from client
+//callback is the response to client
 export async function createTransaction(event, context, callback) {
+  
+  //JSON stored in reqData from API POST event from client
+  // {rate, label_file_type, async}
   const reqData = JSON.parse(event.body);
+  
+  //set rateID to be used as a constant
   const rateId = reqData.rateId;
-  // {parcel, addressTo, addressFrom}
+  
 
   try {
-    //await dynamoDbLib.call("put", params);
+    
+    //create transaction from chosen rateID
     const transaction = await shippo.transaction.create({
       "rate": rateId,
       "label_file_type": "PDF",
       "async": false
   })
+   //on successful API call, send back response of transaction to client
     callback(null, success(transaction));
   } catch (e) {
     callback(null, failure({ status: false }));
